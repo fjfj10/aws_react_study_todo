391c207aa14f75ab1be309d593fadc6083f43f01
fjfj10 <kimmisuk2218@gmail.com>
2023-08-18 17:50:32 +0900

    feat: recoilstate를 이용하여 props를 전달 하지 않고 바로 가져오게 하는 기능 추가

--- src/components/MainSidebar/Header/Header.jsx
+++ src/components/MainSidebar/Header/Header.jsx
@@ -3,19 +3,30 @@ import React from 'react';
 import { css } from "@emotion/react";
 import * as S from "./Style";
 import { HiMenu,HiSearch } from "react-icons/hi";
+import { useRecoilState } from 'recoil';
+import { isSidebarShowState } from "../../../store/sidebarStore";
 
+function Header() {
+    // 직접 접근해 가져오기 때문에 props받을 필요없다
+    const [isSidebarShow, setIsSidebarShow] = useRecoilState(isSidebarShowState);
 
-function Header(props) {
+    const handleMenuToggleClick = () => {
+        setIsSidebarShow(isSidebarShow);
+    }
+
     return (
         <div css={S.SLayout}>
             <div css={S.SContainer}>
                 <h1 css={S.STitle}>Menu</h1>
-                <button css={S.SToggleButton}><HiMenu /></button>
+                <button css={S.SToggleButton} onClick={handleMenuToggleClick}><HiMenu /></button>
             </div>
-            <div css={S.SSearchBox}>
-                <HiSearch css={S.SSearchIcon}/>
-                <input css={S.SSearchInput} type="text" />
-            </div>
+            {isSidebarShow && (
+                <div css={S.SSearchBox}>
+                    <HiSearch css={S.SSearchIcon}/>
+                    <input css={S.SSearchInput} type="text" />
+                </div>
+            )}
+            
         </div>
     );
 }
--- src/components/MainSidebar/MainSidebar.jsx
+++ src/components/MainSidebar/MainSidebar.jsx
@@ -1,16 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
 import * as S from "./Style";
 import Header from './Header/Header';
 import List from './List/List';
+import { useRecoilState } from 'recoil';
+import { isSidebarShowState } from "../../store/sidebarStore";
 
 function MainSidebar(props) {
+    const [isSidebarShow, setIsSidebarShow] = useRecoilState(isSidebarShowState);
+    
+
+    // 이부분은 이후 백앤드에서 서버에서 불러오는걸로 처리 예정 지금은 배열로함
+    const [mainMenuListArray, setMainMenuListArray] = useState({
+        tesks: [
+            {
+                id: 1,
+                path: "/upcomming",
+                icon: "",
+                name: "Upcomming",
+                count: 12
+            },
+            { 
+                id: 2,
+                path: "/today",
+                icon: "",
+                name: "Today",
+                count: 5
+            },
+            {
+                id: 3,
+                path: "/calendar",
+                icon: "",
+                name: "Calendar",
+                count: 0
+            },
+            {
+                id: 4,
+                path: "/stickywall",
+                icon: "",
+                name: "Sticky Wall",
+                count: 0
+            }
+        ],
+        lists: [
+            {
+                id: 1,
+                path: "/personal",
+                icon: "",
+                name: "Personal",
+                count: 0
+            },
+            {
+                id: 2,
+                path: "/work",
+                icon: "",
+                name: "Work",
+                count: 0
+            },
+            {
+                id: 3,
+                path: "/list1",
+                icon: "",
+                name: "List1",
+                count: 0
+            }
+        ]
+    });
+        
+
     return (
-        <div css={S.SLayout} >
+        <div css={S.SLayout(isSidebarShow)}>
             <Header />
-            <List title={"TASKS"}/>
-            <List title={"LISTS"}/>
+            {isSidebarShow && (
+                <>    
+                    <List title={"TASKS"} list={mainMenuListArray.tesks}/>
+                    <List title={"LISTS"} list={mainMenuListArray.lists}/>
+                </>
+            )}
         </div>
         
     );
